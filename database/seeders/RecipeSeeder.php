813fc0b4f2a452bad4782610ce8b65e65b061403
Geist5000 <geist5000@gmail.com>
2022-04-02 17:21:18 +0200

    upgrade to laravel 9.2

--- database/seeders/RecipeSeeder.php
+++ database/seeders/RecipeSeeder.php
@@ -1,12 +1,14 @@
 <?php
 
+namespace Database\Seeders;
+
 use App\Recipe;
 use Illuminate\Database\Seeder;
 
 class RecipeSeeder extends Seeder
 {
     /**
-     * Run the database seeds.
+     * Run the database seeders.
      *
      * @return void
      */
